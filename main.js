caabbc0f0e9dc9d3d7fb52dc65d460dbdbde089d
AliceColombari <colombari.23@gmail.com>
2022-02-01 15:03:30 +0100

    prova codice, problema con visibilità bombe / array

--- main.js
+++ main.js
@@ -28,7 +28,15 @@ function playGame() {
     // ORA RENDO VISIBILE LA GRIGLIA DEL GIOCO
     const mainGrid = document.getElementById('grid');
     mainGrid.classList.remove('hidden');
+
+    // RESETTO IL CAMPO DI GIOCO, STAMPANDO NELL'HTML IL VUOTO
     mainGrid.innerHTML = '';
+
+    // AGGIUNGO CLASSE HIDDEN AL MESSAGGIO DI FINE GIOCO, PER NASCONDERLO
+    document.getElementById('endgame').classList.add('hidden');
+
+    // IMPOSTO UNA VARIABILE PER IL NUMERO DELLE BOMBE 
+    const maxBombNumber = 16;
 
     // SCELTA LIVELLO DA PARTE DEL GIOCATORE - SCELTA
     const userDifficulty = document.getElementById('leveltype').value;
@@ -54,6 +62,20 @@ function playGame() {
     }
 
 
+    // COSTANTE PER RICHIAMARE FUNZIONE, PER GENERARE LE BOMBE, 
+    // METTENDO IL NUMERO DI BOMBE DESIDERATE E IL NUMERO DI CELLE
+    const bombComeArray = generateBombComeArray(numbersOfCells, maxBombNumber);
+    console.log(bombComeArray);
+
+    // CALCOLO IL NUMERO MASSIMO DI TENTATIVI CHE L'UTENTE PU0' FARE 
+    const maxAttemps = parseInt(numbersOfCells - bombComeArray.lenght);
+    console.log(maxAttemps);
+
+    // CREO UN ARRAY DOVE INSERISCO I TENTATIVI DELL'UTENTE - CELLE CLICCATE
+    const rightAttemps = [];
+    console.log(rightAttemps);
+
+
     // CREO LE CELLE IN BASE ALLA SCELTA DEL LIVELLO SELEZIONATO
     // CICLO FOR PER FAR FUNZIONARE L'AZIONE
     for( let i = 1; i <= numbersOfCells; i++ ) {
@@ -65,29 +87,121 @@ function playGame() {
         mainGrid.appendChild(newGeneratedCell);
     }
     
+    // FUNZIONE PER IL CLICK SULLA CELLA SCELTA
+    function cellsClick() {
+        // LEGGO IL VALORE DELLA CELLA CHE L'UTENTE HA CLICCATO
+        const clickNumber = parseInt(this.querySelector('span').textContent);
+        console.log('Al momento hai cliccato su: ' + clickNumber);
+
+        // SE IL NUMERO E' PRESENTA NELLA LISTA DEGLI ARRAY / BOMBE GENERATE
+        if (bombComeArray.includes(clickNumber)) {
+            // LA CELLA SI COLORA DI ROSSO
+            // E' STATA SELEZIONATA UNA CELLA BOMBA
+            this.classList.add('red-bomb')
+            endGame('perdita')
+            // ALTRIMENTI 
+        } else {
+            // LA CELLA SI COLORA DI AZZURRO
+            this.classList.add('active');
+            // L'UTENTE CONTINUA IL GIOCO E CONTINUA A CLICCARE SULLE CELLE, MA NON SU QUELLE CLICCATE
+            this.style.pointerEvents = "none";
+            // IL NUMERO CLICCATO VIENE AGGIUNTO NELLA LISTA DEGLI ARRAY DELLE CELLE CLICCATE DALL'UTENTE
+            rightAttemps.push(clickNumber);
+
+            // TENTAIVI MASSIMI RAGGIUNTI, IL GIOCO FINISCE E L'UTENTE HA VINTO
+            if (rightAttemps.lenght >= maxAttemps) {
+                endGame('vittoria')
+            }
+
+        }
+
+    }
+
+    // FUNZIONE CHE DECRETA LA FINE DEL GIOCO, SE L'UTENTE HA PERSO O VINTO
+    function endGame(result) {
+        // VARIABILE PER IL MESSAGGIO DI FINE GIOCO
+        let resultMessage;
+
+        // CONDIZIONE PER DECRETARE LA VITTORIA
+        if (result == 'vittoria') {
+            // SE L'UTENTE HA VINTO - MESSAGGIO
+            resultMessage = 'Congratulazioni sei il nuovo vincitore!'
+        } else {
+            // ALTRIMENTI L'UTENTE HA PERSO - MESSAGGIO
+            resultMessage = 'Mi dispiace, hai perso! Hai effettuato ' + rightAttemps.lenght + ' tentativi!'
+        }
+
+        // MESSAGGIO DI FINE GIOCO
+        const endGameText = document.getElementById('endgame');
+        console.log('result' + endGameText);
+        // STAMPA NELL'HTML IL RISULTATO
+        endGameText.innerHTML = resultMessage;
+        // RIMUOVO CLASSE HIDDEN PER RENDERE VISIBILE IL MESSAGGIO FINALE
+        endGameText.classList.remove('hidden');
+
+        // DECRETATA LA VITTORIA, IL GIOCO FINISCE
+        // LE CELLE NON POSSONO PIU' ESSERE CLICCABIOLI, NESSUNA
+        // DEVO SCEGLIERE IL LIVELLO E CLICCARE PLAY, RICOMINCIO, LANCIO UNA NUOVA PARTITA
+        const allCells = document.getElementsByClassName('singlecell');
+
+        // CICLO FOR PER POTER RICOMINCIARE
+        for ( let i = 0; i < allCells.lenght; i++) {
+            const thisCell = allCells[i];
+            thisCell.style.pointerEvents = "none";
+        }
+
+    }
+
 }
 
 
-    // FUNZIONE CAMBIO DEL COLORE 
-    function cellsClick() {
-        // AL CLICK SULLA CELLA SCELTA CAMBIO IL COLORE ATTIVANDO LA CLASSE ACTIVE
-        this.classList.add('active');   
-    }
-    
-    // FUNZIONE CHE CREA GLI ELEMENTI ALL'INTERNO DELLA GRIGLIA + ARGOMENTI NUMERO E DIMENSIONE CELLA
-    function generateCellsItem(number, cellDimention) {
-        // VARIABILE CHE GENERA UN DIV - CONTENITORE CELLA DELLA GRIGLIA
-        const newCell = document.createElement('div');
-        // RICHIAMO VARIABILE E ASSEGNO LA CLASSE PRESA DAL CSS PER LA SINGOLA CELLA DELLA GRIGLIA
-        newCell.classList.add('singlecell');
-        // GENERO IL NUMERO DA INSERIRE ALL'INTERNO DELLA CELLA 
-        newCell.innerHTML = `<span>${number}</span>`; 
+// CREARE LE BOMBE PER FAR FUNZIONARE IL CAMPO MINATO
+// FUNZIONE CHE GENERA UN ARRAY DI 16 BOMBE / NUMERI CASUALI
+function generateBombComeArray (maxNumber, maxBombNumber) {
+    // CREO ARRAY VUOTO PER GENERARE LE BOMBE
+    let bombGenerateArray = [];
 
-        // IMPOSTO ALTEZZA E LARGHEZZA IN BASE ALLA DIMENSIONE DELLA CELLA SCELTA IN BASE AL LIVELLO SELEZIONATO
-        // RICHIAMO VARIABILE NEWCELL E ASSEGNO CON LO STILE LE CARATTERISTICHE W + H
-        // IN AUTOMATICO ANDRANNO A SELEZIONARSI LE DIMENSIONI DATE NEL CSS || CELL-EASY/HARD/CRAZY
-        newCell.style.width = `calc(100% / ${cellDimention})`;
-        newCell.style.height = `calc(100% / ${cellDimention})`;
-        // FINE FUNZIONE E TORNA A NEWCELL, VARIABILE INIZIALE
-        return newCell;
+    while (bombGenerateArray.lenght < maxBombNumber) {
+        const bombNumberRandom = getRandomNumber(1, maxNumber);
+
+        // SE bombGenerateArray NON INCLUDE UNA BOMBA, ALLORA CONTINUA
+        if (!bombGenerateArray.includes(bombNumberRandom)) {
+            // AGGIUNGO UN VALORE ALL'ARRAY CREATO
+            bombGenerateArray.push(bombNumberRandom);
+        }
     }
+
+    return bombGenerateArray;
+}
+
+
+
+// FUNZIONE CHE CREA GLI ELEMENTI ALL'INTERNO DELLA GRIGLIA + ARGOMENTI NUMERO E DIMENSIONE CELLA
+function generateCellsItem(number, cellDimention) {
+    // VARIABILE CHE GENERA UN DIV - CONTENITORE CELLA DELLA GRIGLIA
+    const newCell = document.createElement('div');
+    // RICHIAMO VARIABILE E ASSEGNO LA CLASSE PRESA DAL CSS PER LA SINGOLA CELLA DELLA GRIGLIA
+    newCell.classList.add('singlecell');
+    // GENERO IL NUMERO DA INSERIRE ALL'INTERNO DELLA CELLA 
+    newCell.innerHTML = `<span>${number}</span>`; 
+
+    // IMPOSTO ALTEZZA E LARGHEZZA IN BASE ALLA DIMENSIONE DELLA CELLA SCELTA IN BASE AL LIVELLO SELEZIONATO
+    // RICHIAMO VARIABILE NEWCELL E ASSEGNO CON LO STILE LE CARATTERISTICHE W + H
+    // IN AUTOMATICO ANDRANNO A SELEZIONARSI LE DIMENSIONI DATE NEL CSS || CELL-EASY/HARD/CRAZY
+    // newCell.style.width = `calc(100% / ${cellDimention})`;
+    // newCell.style.height = `calc(100% / ${cellDimention})`;
+
+    // DIMENSIONE CELLE CALCOLATA DIRETTAMENTE CON JS
+    const dimensione = `calc(100% / ${cellDimention})`
+    newCell.style.width = dimensione;
+    newCell.style.height = dimensione;
+
+    // FINE FUNZIONE E TORNA A NEWCELL, VARIABILE INIZIALE
+    return newCell;
+}
+
+
+// FUNZIONE CHE GENERA UN NUMERO CAUSALE
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
